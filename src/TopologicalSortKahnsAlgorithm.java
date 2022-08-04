c8129c14977a0ac135e0d5ead242d7e457a506c7
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 20:02:07 -0700

    coded the method for the topological algorithm, basically we are just adding the connections in inDegree first form the given adj list of lists. then we check if the inDegree of a particular element is 0 then put it in queue, and then put it in answer as well but, then also check for its connections, and add them in queue as long as they are being put in the queue

--- src/TopologicalSortKahnsAlgorithm.java
+++ src/TopologicalSortKahnsAlgorithm.java
@@ -1,5 +1,50 @@
+import java.util.*;
+
 public class TopologicalSortKahnsAlgorithm {
+
+
+
+    static int [] topologicalSortAlgorithm(ArrayList<ArrayList<Integer>> adj, int v){
+        int []ans = new int[v];
+        int index =0;
+        int [] inDegree = new int[v];
+        for(int i=0; i<v; i++){
+            inDegree[i]=0;
+        }
+
+
+        for(int i=0; i<v; i++){
+            for(int j=0; j<adj.get(i).size(); i++){
+                int connection = adj.get(i).get(j);
+                inDegree[connection] ++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<v; i++){
+            if(inDegree[i]==0){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int current = q.remove();
+            ans[index] = current;
+            index++;
+            for(int i=0; i<adj.get(current).size(); i++){
+                int connection = adj.get(current).get(i);
+                inDegree[connection] --;
+
+                if(inDegree[connection]==0){
+                    q.add(connection);
+                }
+            }
+            return ans;
+        }
+    }
+
+
     public static void main(String[] args) {
-        
+
     }
 }
